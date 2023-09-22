02cfc2462ec050d3cfa5b0be7d6887196fb49448
Kirill <91202716+DON-BOLSHOF@users.noreply.github.com>
2023-09-22 17:34:32 +0500

    NavAI Package add

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private Transform _player;
+    private NavMeshAgent _agent;
+
+    private void Start()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+    }
+}
